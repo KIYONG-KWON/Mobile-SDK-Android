8e0fb80a99e8b8cbaba0b5328acfa09466f73dc2
KIYONG-KWON <kkyong@dsme.co.kr>
2021-06-08 14:53:49 +0900

    Revert "Test Push"
    This reverts commit 1a0fd2956ad5f3dcdef6f64bec8c956764c611d8.

--- Sample Code/app/src/main/java/com/dji/sdk/sample/internal/controller/MainActivity.java
+++ Sample Code/app/src/main/java/com/dji/sdk/sample/internal/controller/MainActivity.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -26,13 +25,7 @@ import com.dji.sdk.sample.internal.view.DemoListView;
 import com.dji.sdk.sample.internal.view.PresentableView;
 import com.squareup.otto.Subscribe;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.SocketException;
 import java.util.Stack;
-import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -55,25 +48,7 @@ public class MainActivity extends AppCompatActivity {
     private SearchView searchView;
     private MenuItem searchViewItem;
     private MenuItem hintItem;
-    TextView packetView, rcvIpText, rcvPortText; // 추가
-    String rcvIp, rcvPort, rcvPacket;
-    //private final Handler mHandler = new Handler();
 
-    //데이터를 송신받을떄 실행할 코드
-    private final Runnable rnb = new Runnable() {
-        @Override
-        public void run() {
-            //레이아웃에 존재하는 UI를 불러옴
-            //packetView = findViewById(R.id.packetView);
-            //rcvIpText = findViewById(R.id.ipView);
-            //rcvPortText = findViewById(R.id.portView);
-
-            //UI에 UDP 수신 하면서 얻은 정보를 업데이트 함
-            //.setText(rcvPacket);
-            //rcvIpText.setText(rcvIp);
-            //rcvPortText.setText(rcvPort);
-        }
-    };
     //region Life-cycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,10 +58,6 @@ public class MainActivity extends AppCompatActivity {
         setupActionBar();
         contentFrameLayout = (FrameLayout) findViewById(R.id.framelayout_content);
         initParams();
-
-        //UDP 수신 쓰레드 설정 및 시작부분
-        ReceiveData rcvServer = new ReceiveData(9990);
-        rcvServer.start();
     }
 
     @Override
@@ -336,57 +307,4 @@ public class MainActivity extends AppCompatActivity {
     public static class ConnectivityChangeEvent {
     }
     //endregion
-    class ReceiveData extends Thread{
-        //UDP로 패킷을 받았을때 받은 패킷을 UI로 올리기 위해 handler 받아옴
-        //Handler handler = mHandler;
-        DatagramSocket rSocket = new Socket("127.0.0.1",9990);
-
-        //UDP 수신 포트를 열기(원하는 port 아무거나)
-        public ReceiveData(int port){
-            try {
-                //수신 소켓 생성
-                rSocket = new DatagramSocket(port);
-            } catch (SocketException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void run(){
-            try{
-                while (true){
-                    //받을 패킷 생성
-                    byte[] buf = new byte[1024];
-                    //패킷으로 변경 (바이트 버퍼, 버퍼길이)
-                    DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                    //데이터 수신 대기
-                    rSocket.receive(packet);
-                    //패킷을 보낸 상대방의 ip를 저장함
-                    InetAddress ina = packet.getAddress();
-                    rcvIp = ina.toString();
-                    //패킷을 보낸 상대방의 port를 저장함
-                    int inp = packet.getPort();
-                    rcvPort = String.valueOf(inp);
-                    //수신받은 데이터를 문자열로 변환
-                    rcvPacket = new String(buf);
-                    //에코하기 위해 송신용 패킷을 만듦 (받은 패킷, 패킷 길이, 상대방 IP, 상대방 PORT)
-                    packet = new DatagramPacket(buf, buf.length, ina, inp);
-                    //이 기기로 UDP송신을 한 상대방에게 받은패킷을 돌려줌
-                    rSocket.send(packet);
-                    //데이터를 받았다면 UI로 표현해주기 위해 runnable 사용
-                    //handler.post(rnb);
-                    //쓰레드를 인터럽트로 종료시키기 위해 sleep을 사용함
-                    sleep(20);
-                }
-            }catch (InterruptedException e){
-                rcvPacket = e.toString();
-                //handler.post(rnb);
-            }
-            catch (Exception e){
-                rcvPacket = e.toString();
-                //handler.post(rnb);
-            }
-        }
-    }
-
-
 }
